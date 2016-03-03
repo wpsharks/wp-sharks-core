2e474290b13a933e5031bf9d8d95763c2b1b5407
jaswsinc <jaswsinc@wsharks.com>
2016-03-03 14:52:25 -0900

    Enhancing stubs.

--- src/includes/plugin.php
+++ src/includes/plugin.php
@@ -8,7 +8,6 @@ if (!defined('WPINC')) {
     exit('Do NOT access this file directly: '.basename(__FILE__));
 }
 require __DIR__.'/rv.php'; // Getup `wp_php_rv` config.
-
 if (require(dirname(__DIR__).'/vendor/websharks/wp-php-rv/src/includes/check.php')) {
     add_action('plugins_loaded', function () {
         require_once __DIR__.'/stub.php';
--- src/includes/uninstall.php
+++ src/includes/uninstall.php
@@ -8,7 +8,6 @@ if (!defined('WPINC')) {
     exit('Do NOT access this file directly: '.basename(__FILE__));
 }
 require __DIR__.'/rv.php'; // Getup `wp_php_rv` config.
-
 if (require(dirname(__DIR__).'/vendor/websharks/wp-php-rv/src/includes/check.php')) {
     require_once __DIR__.'/stub.php';
     new App(['§uninstall' => true]);
