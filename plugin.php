b490b3e64e8fda46f34b2a7622961035ac6d8456
jaswsinc <jaswsinc@wsharks.com>
2016-04-28 19:48:15 -0800

    Updating core.

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  *
  * @wp-plugin
  *
- * Version: 160428
+ * Version: 160429
  * Text Domain: wp-sharks-core
  * Plugin Name: WP Sharks Core
  *
--- src/includes/classes/App.php
+++ src/includes/classes/App.php
@@ -25,7 +25,7 @@ class App extends CoreClasses\App
      *
      * @type string Version.
      */
-    const VERSION = '160428'; //v//
+    const VERSION = '160429'; //v//
 
     /**
      * Constructor.
