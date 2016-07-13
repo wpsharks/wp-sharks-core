7273e2646c9a758df4e943983a1f0ac15601b175
jaswsinc <jaswsinc@wsharks.com>
2016-07-13 08:31:39 -0800

    Adding About tab content.

--- src/includes/templates/s-core/menu-pages/dashboard/about.php
+++ src/includes/templates/s-core/menu-pages/dashboard/about.php
@@ -22,7 +22,7 @@ extract($this->current_vars); // Template variables.
 </h3>
 
 <p>
-    <?= sprintf(__('<a href="%2$s" target="_blank">%1$s</a> was founded by business partners <a href="%3$s" target="_blank" class="-no-icon">Jason Caldwell</a> and <a href="%4$s" target="_blank" class="-no-icon">Raam Dev</a>. Over the years we\'ve built everything from simple contact forms, to ecommerce platforms, cache engines, social-media tools, and more. <em style="display:inline-block; margin-left:.5em;">Always on top of WordPress.</em>', 'wp-sharks-core'), esc_html($this->App::CORE_CONTAINER_NAME), esc_url($this->s::coreUrl('/')), esc_url($this->s::coreUrl('/vendor/jaswsinc')), esc_url($this->s::coreUrl('/vendor/raamdev'))); ?>
+    <?= sprintf(__('<a href="%2$s" target="_blank">%1$s</a> was founded by business partners <a href="%3$s" target="_blank">Jason Caldwell</a> and <a href="%4$s" target="_blank">Raam Dev</a>. Over the years we\'ve built everything from simple contact forms, to ecommerce platforms, cache engines, social-media tools, and more. <em style="display:inline-block; margin-left:.5em;">Always on top of WordPress.</em>', 'wp-sharks-core'), esc_html($this->App::CORE_CONTAINER_NAME), esc_url($this->s::coreUrl('/')), esc_url($this->s::coreUrl('/vendor/jaswsinc')), esc_url($this->s::coreUrl('/vendor/raamdev'))); ?>
 </p>
 
 <p>
@@ -30,7 +30,7 @@ extract($this->current_vars); // Template variables.
 </p>
 
 <p>
-    <?= sprintf(__('A developer\'s life is filled with fun and we all love what we do. We find great pleasure in working with each other, with our customers, and with others in the WordPress community. While there is distance between us, we\'re a really tight group (good at online collaboration), and we work very hard on common goals; e.g., to produce great software and help others. We pride ourselves on our level of <a href="%1$s" target="_blank" class="-no-icon">customer support</a> and response time.', 'wp-sharks-core'), esc_url($this->s::coreUrl('/support'))); ?>
+    <?= sprintf(__('A developer\'s life is filled with fun and we all love what we do. We find great pleasure in working with each other, with our customers, and with others in the WordPress community. While there is distance between us, we\'re a really tight group (good at online collaboration), and we work very hard on common goals; e.g., to produce great software and help others. We pride ourselves on our level of <a href="%1$s" target="_blank">customer support</a> and response time.', 'wp-sharks-core'), esc_url($this->s::coreUrl('/support'))); ?>
 </p>
 
 <p style="font-size:110%; font-style:italic;">
