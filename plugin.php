252e5ab104bfda9dca2ef3a426746474731a494e
jaswrks <jas@src.works>
2017-04-06 01:06:00 -0800

    Updating build.

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  *
  * @wp-plugin
  *
- * Version: 170406.31578
+ * Version: 170406.32657
  * Text Domain: wp-sharks-core
  * Plugin Name: WP Sharks Core
  *
--- src/includes/classes/App.php
+++ src/includes/classes/App.php
@@ -47,7 +47,7 @@ class App extends CoreClasses\App
      *
      * @type string Version.
      */
-    const VERSION = '170406.31578'; //v//
+    const VERSION = '170406.32657'; //v//
 
     /**
      * ReST action API version.
